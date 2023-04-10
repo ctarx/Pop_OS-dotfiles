99de7a23ed915614ec1db9d9b7603f7c33d4af95
ctarx <ctarx@protonmail.com>
2023-04-10 07:16:07 +0100

    nvim-colorizer.lua settings

--- config/.config/lvim/config.lua
+++ config/.config/lvim/config.lua
@@ -205,16 +205,17 @@ lvim.plugins = {
   },
   {
     "norcalli/nvim-colorizer.lua",
-
-    -- Attach to certain Filetypes, add special configuration for `html`
-    -- Use `background` for everything else.
-    require 'colorizer'.setup {
-      'css';
-      'javascript';
-      html = {
-        mode = 'foreground';
-      }
-    }
+      config = function()
+        require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+          RGB = true, -- #RGB hex codes
+          RRGGBB = true, -- #RRGGBB hex codes
+          RRGGBBAA = true, -- #RRGGBBAA hex codes
+          rgb_fn = true, -- CSS rgb() and rgba() functions
+          hsl_fn = true, -- CSS hsl() and hsla() functions
+          css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+          css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+          })
+    end,
   },
   {
     "windwp/nvim-ts-autotag",
