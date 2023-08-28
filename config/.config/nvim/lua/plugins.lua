f5985f94d4b13f38540add543119c4920f61affc
ctarx <ctarx@protonmail.com>
2023-08-28 06:51:30 +0100

    neovim plugins

--- config/.config/nvim/lua/plugins.lua
+++ config/.config/nvim/lua/plugins.lua
@@ -12,28 +12,31 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugin_list = {
-    { 'numToStr/Comment.nvim', opts = {} },
+  { 
+    'numToStr/Comment.nvim', 
+      opts = {} 
+  },
 
-    {
-        'numToStr/Comment.nvim',
-        config = function()
-            require("Comment").setup()
-        end
-    },
+  {
+    'numToStr/Comment.nvim',
+      config = function()
+        require("Comment").setup()
+      end
+  },
 
-    {
-        'ellisonleao/gruvbox.nvim',
-        priority = 1000,
-        config = function()
-            vim.cmd("colorscheme gruvbox")
-        end
-    },
+  {
+    'ellisonleao/gruvbox.nvim',
+      priority = 1000,
+      config = function()
+        vim.cmd("colorscheme gruvbox")
+      end
+  },
 
-   {
-        'akinsho/bufferline.nvim',
-        version = "*",
-        dependencies = 'nvim-tree/nvim-web-devicons'
-   },
+  {
+    'akinsho/bufferline.nvim',
+      version = "*",
+      dependencies = 'nvim-tree/nvim-web-devicons'
+  },
   -- LSP
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
@@ -42,64 +45,63 @@ local plugin_list = {
     'folke/neodev.nvim',
 
   -- Autocompletion
-    {
-        'hrsh7th/nvim-cmp',
-        dependencies = {
-            'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip',
-            'rafamadriz/friendly-snippets',
+  {
+    'hrsh7th/nvim-cmp',
+      dependencies = {
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip',
+        'rafamadriz/friendly-snippets',
             
-            'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lsp',
             
-        },
-    },
+      },
+  },
 
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-    },
+  {
+    'nvim-treesitter/nvim-treesitter',
+      build = ':TSUpdate',
+  },
 
+  {
+    'nvim-telescope/telescope.nvim',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+  },
 
-    {
-        'nvim-telescope/telescope.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+  },
 
-    {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-    },
+  {
+    'nvim-tree/nvim-tree.lua',
+      dependencies = 'nvim-tree/nvim-web-devicons',
+  },
 
-    {
-        'nvim-tree/nvim-tree.lua',
-        dependencies = 'nvim-tree/nvim-web-devicons',
-    },
+  {
+    'nvim-lualine/lualine.nvim',
+      dependencies = {
+        "nvim-tree/nvim-web-devicons"
+      },
+      config = function()
+        require("lualine").setup({
+          icons_enabled = true,
+          theme = 'gruvbox',
+        })
+      end,
+  },
 
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = {
-            "nvim-tree/nvim-web-devicons"
-        },
-        config = function()
-            require("lualine").setup({
-                icons_enabled = true,
-                theme = 'onedark',
-            })
-        end,
-    },
-
-    {'lukas-reineke/indent-blankline.nvim'},
-    {'windwp/nvim-autopairs'},
-    {'norcalli/nvim-colorizer.lua'},
-    {
-      'iamcco/markdown-preview.nvim',
+   'lukas-reineke/indent-blankline.nvim',
+   'windwp/nvim-autopairs',
+   'norcalli/nvim-colorizer.lua',
+  {
+    'iamcco/markdown-preview.nvim',
       ft = 'markdown',
       build = function()
         vim.fn['mkdp#util#install']()
       end,
-    }
-
   }
+
+}
 
 local opts = {}
 
