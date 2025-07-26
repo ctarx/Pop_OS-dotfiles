e615abebd623a5f99c90a579ede61ce060ccfb12
ctarx <ctarx@protonmail.com>
2025-07-26 08:30:04 +0100

    Update wezterm and .profile settings

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -1,93 +1,67 @@
 local wezterm = require("wezterm")
+local config = {}
 
-return {
-	-- GENERAL --------------------------------------------------------
-	enable_wayland = false,
-	automatically_reload_config = true,
+-- General
+config.enable_wayland = false
+config.window_decorations = "NONE"
+config.enable_tab_bar = false
+config.window_background_opacity = 0.90
+config.window_padding = { left = 12, right = 12, top = 10, bottom = 10 }
 
-	-- ENVIRONMENT ----------------------------------------------------
-	set_environment_variables = {
-		TERM = "xterm-256color",
-	},
+-- Env
+config.set_environment_variables = {
+	TERM = "xterm-256color",
+}
 
-	-- WINDOW ---------------------------------------------------------
-	window_decorations = "NONE",
-	enable_tab_bar = false,
-	window_background_opacity = 0.90,
-	window_padding = {
-		left = 12,
-		right = 12,
-		top = 10,
-		bottom = 10,
-	},
+-- Scrollback
+config.scrollback_lines = 10000
 
-	-- SCROLLBACK -----------------------------------------------------
-	scrollback_lines = 10000,
+-- Bell
+config.audible_bell = "Disabled"
+config.visual_bell = {
+	fade_in_function = "Linear",
+	fade_out_function = "Linear",
+	fade_in_duration_ms = 20,
+	fade_out_duration_ms = 20,
+}
 
-	-- BELL -----------------------------------------------------------
-	audible_bell = "Disabled",
-	visual_bell = {
-		fade_in_function = "Linear",
-		fade_out_function = "Linear",
-		fade_in_duration_ms = 20,
-		fade_out_duration_ms = 20,
-	},
+-- Keybindings
+config.keys = {
+	{ key = "F11", mods = "", action = wezterm.action.ToggleFullScreen },
+	{ key = "V", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
+	{ key = "C", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
+}
 
-	-- KEYBINDINGS ----------------------------------------------------
-	keys = {
-		{ key = "F11", action = wezterm.action.ToggleFullScreen },
-	},
-
-	-- MOUSE BINDINGS -------------------------------------------------
-	mouse_bindings = {
-		{
-			event = { Up = { streak = 1, button = "Left" } },
-			mods = "",
-			action = wezterm.action.CopyTo("ClipboardAndPrimarySelection"),
-		},
-	},
-
-	-- CURSOR ---------------------------------------------------------
-	default_cursor_style = "BlinkingBlock",
-	cursor_blink_rate = 750,
-	cursor_thickness = 0.15,
-
-	-- MOUSE ----------------------------------------------------------
-	hide_mouse_cursor_when_typing = false,
-
-	-- FONT -----------------------------------------------------------
-	font = wezterm.font_with_fallback({
-		{ family = "JetBrainsMono Nerd Font Mono", weight = "Regular", italic = false },
-	}),
-	font_size = 14.0,
-
-	-- COLORS ---------------------------------------------------------
-	colors = {
-		foreground = "#ebdbb2",
-		background = "#282828",
-
-		cursor_bg = "#cc241d",
-		cursor_fg = "#282828",
-
-		ansi = {
-			"#282828",
-			"#cc241d",
-			"#98971a",
-			"#d79921",
-			"#458588",
-			"#b16286",
-			"#689d6a",
-			"#a89984",
-		},
-		brights = {
-			"#928374",
-			"#fb4934",
-			"#b8bb26",
-			"#fabd2f",
-			"#83a598",
-			"#d3869b",
-			"#8ec07c",
-			"#ebdbb2",
-		},
+-- Mouse
+config.mouse_bindings = {
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "",
+		action = wezterm.action.CopyTo("ClipboardAndPrimarySelection"),
 	},
 }
+
+-- Cursor
+config.default_cursor_style = "BlinkingBlock"
+config.animation_fps = 60
+config.cursor_blink_rate = 500
+config.cursor_blink_ease_in = "EaseIn"
+config.cursor_blink_ease_out = "EaseOut"
+config.force_reverse_video_cursor = false
+
+-- Font
+config.font = wezterm.font_with_fallback({
+	{ family = "JetBrainsMono Nerd Font Mono", weight = "Regular", italic = false },
+})
+config.font_size = 14.0
+
+-- Colorscheme
+config.color_scheme = "GruvboxDark"
+
+-- Override cursor color
+config.colors = {
+	cursor_bg = "#cc241d",
+	cursor_fg = "#282828",
+}
+
+return config
